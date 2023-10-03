155fc05b8cd86f3d9aa0de753f1e7ef9bb9987d7
Tim Kuijpers <timkuijpers2002@outlook.com>
2023-10-03 12:50:40 +0200

    added services

--- GatewayBenchmark/Services/ToDoService.cs
+++ GatewayBenchmark/Services/ToDoService.cs
@@ -1,6 +1,7 @@
 ﻿using GatewayBenchmark.Data;
 using GatewayBenchmark.Models;
 using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
 
 namespace GatewayBenchmark.Services
 {
@@ -29,7 +30,73 @@ namespace GatewayBenchmark.Services
 
             return await Task.FromResult(new CreateToDoResponse
             {
-                Id = toDoItem.Id,
+                Id = toDoItem.Id
+            });
+        }
+
+        public override async Task<ReadToDoResponse> ReadToDo(ReadToDoRequest request, ServerCallContext context)
+        {
+            if(request.Id <= 0)
+            {
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Resource index must be greater than 0"));
+            }
+            var toDoItem = await _dbContext.ToDoItems.FirstOrDefaultAsync(t => t.Id == request.Id);
+
+            if(toDoItem != null) 
+            {
+                return await Task.FromResult(new ReadToDoResponse 
+                { 
+                    Id = toDoItem.Id,
+                    Title = toDoItem.Title,
+                    Description = toDoItem.Description,
+                    Status = toDoItem.Status
+                });
+            }
+
+            throw new RpcException(new Status(StatusCode.NotFound, $"No task with id {request.Id}"));
+        }
+
+        public override async Task<GetAllToDoResponse> GetAllToDo(GetAllToDoRequest request, ServerCallContext context)
+        {
+            var response = new GetAllToDoResponse();
+            var toDoItems = await _dbContext.ToDoItems.ToListAsync();
+
+            foreach( var toDoItem in toDoItems)
+            {
+                response.ToDo.Add(new ReadToDoResponse
+                {
+                    Id = toDoItem.Id,
+                    Title = toDoItem.Title,
+                    Description = toDoItem.Description,
+                    Status = toDoItem.Status
+                });
+            }
+
+            return await Task.FromResult(response);
+        }
+
+        public override async Task<UpdateToDoResponse> UpdateToDo(UpdateToDoRequest request, ServerCallContext context)
+        {
+            if (request.Id <= 0 || request.Title == string.Empty || request.Description == string.Empty)
+            {
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "You must provide a valid input"));
+            }
+            var toDoItem = await _dbContext.ToDoItems.FirstOrDefaultAsync(t => t.Id == request.Id);
+
+            if (toDoItem == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"No task with id {request.Id}"));
+            }
+
+            toDoItem.Title = request.Title;
+            toDoItem.Description = request.Description;
+            toDoItem.Status = request.Status;
+
+            await _dbContext.SaveChangesAsync();
+
+            return await Task.FromResult(new UpdateToDoResponse
+            {
+                Id = toDoItem.Id
             });
         }
     }
