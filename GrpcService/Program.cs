d4da54f4e2b9a3c149958f76158565ed336aecf2
TimKuijpers2002 <timkuijpers2002@outlook.com>
2023-10-04 13:11:42 +0200

    added correct docker-compose

--- GrpcService/Program.cs
+++ GrpcService/Program.cs
@@ -6,7 +6,11 @@ using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
+var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User ID=sa;Password={dbPassword};TrustServerCertificate=True";
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(connectionString));
 
 builder.Services.AddGrpc().AddJsonTranscoding();
 
